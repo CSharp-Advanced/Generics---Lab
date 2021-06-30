615b5a229d229bdbb03ce1bb2c35b038bcb39be5
remzi chaushev <remzitdf@abv.bg>
2021-06-30 11:51:41 +0300

    Add project files.

---
+++ 01.BoxOfT/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private readonly Stack<T> data;
+
+        public Box()
+        {
+            data = new Stack<T>();
+        }
+
+        public int Count => data.Count;
+
+        public void Add(T element)
+        {
+            data.Push(element);
+        }
+
+        public T Remove()
+        {
+            return data.Pop();
+        }
+    }
+}
